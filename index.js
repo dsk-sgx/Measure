d854783de1a28c568c26c86f7b346659822adad1
daisuke <dsk.sgx@gmail.com>
2019-02-09 20:17:35 +0900

    getのエンドポイントを追加

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use('/', express.static('public'));
 
+app.get('/', function (req, res) {
+  res.send('index.js');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
